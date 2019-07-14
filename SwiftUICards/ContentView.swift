1a6d917303104957170b14b9ca474e682a34fdd0
Wesley S. Favarin <wesleysfavarin@hotmail.com>
2019-07-14 13:34:25 -0300

    SwiftUI Cards Comleted application

---
+++ SwiftUICards/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUICards
+//
+//  Created by wesley s favarin on 14/07/19.
+//  Copyright © 2019 wesley s favarin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
